6681230733d927ec51ad9508de64ded77554595d
Pawel Lechowicz <plechowicz@endego.com>
2021-11-16 16:31:40 +0100

    Poprawa krok_0, pobranie przykladow z https://valelab4.ucsf.edu/svn/3rdpartypublic/boost/doc/html/boost_asio/examples.html

--- src/server/krok_0.cpp
+++ src/server/krok_0.cpp
@@ -107,19 +107,28 @@ private:
       );
   }
 
+void show_data(const char* name, const char* p, std::size_t size)
+{
+    std::cout << name << " = ";
+    for (std::size_t i = 0; i < size; ++i)
+        std::cout << p[i];
+    std::cout << '\n';
+}
+
 int check_request( std::size_t bytes_transferred )
 {
-  //auto self = shared_from_this();
+  auto self = shared_from_this();
+
   Request request;
   HttpRequestParser parser;
 
-  std::string str_data_from_server(data_from_server_.data(), bytes_transferred);
-  const char* request_text = str_data_from_server.c_str();
+  const char* request_text = data_from_client_.data();
 
   std::cout << "check_request( " << bytes_transferred << " ) " << std::endl;
-  std::cout << "request_text = " << request_text << std::endl;
-  
-  HttpRequestParser::ParseResult res = parser.parse(request, request_text, request_text + sizeof(request_text));
+ 
+  self->show_data( "request_text", request_text, bytes_transferred ); // data_from_client_.size());
+ 
+  HttpRequestParser::ParseResult res = parser.parse(request, request_text, request_text + bytes_transferred);
 
   if( res == HttpRequestParser::ParsingCompleted )
   {
@@ -129,31 +138,24 @@ int check_request( std::size_t bytes_transferred )
   else
   {
       std::cerr << "Parsing failed" << std::endl;
-      std::cerr << "bytes_transferred from server = " << bytes_transferred << std::endl;
+      std::cerr << "bytes_transferred from client = " << bytes_transferred << std::endl;
       return EXIT_FAILURE;
   }
 }
 
 int check_response( std::size_t bytes_transferred )  // char* response_text )
 {
-    // const char response_text[] =
-    //         "HTTP/1.1 200 OK\r\n"
-    //         "Server: nginx/1.2.1\r\n"
-    //         "Content-Type: text/html\r\n"
-    //         "Content-Length: 8\r\n"
-    //         "Connection: keep-alive\r\n"
-    //         "\r\n"
-    //         "<html />";
-
-    Response response;
-    HttpResponseParser parser;
-
-  std::string str_data_from_client(data_from_client_.data(), bytes_transferred);
-  const char* response_text = str_data_from_client.c_str();
+  auto self = shared_from_this();
   
+  Response response;
+  HttpResponseParser parser;
+
+  const char* response_text = msg_OK_.data(); // data_from_server_.data();
+
   std::cout << "check_response( " << bytes_transferred << " ) " << std::endl;
-  std::cout << "response_text = " << response_text << std::endl;
-  
+ 
+  self->show_data( "response_text", response_text, bytes_transferred ); 
+ 
   HttpResponseParser::ParseResult res = parser.parse(response, response_text, response_text + sizeof(response_text));
 
   if( res == HttpResponseParser::ParsingCompleted )
@@ -164,7 +166,7 @@ int check_response( std::size_t bytes_transferred )  // char* response_text )
   else
   {
       std::cerr << "Parsing failed" << std::endl;
-      std::cerr << "bytes_transferred from client = " << bytes_transferred << std::endl;
+      std::cerr << "bytes_transferred from server = " << bytes_transferred << std::endl;
       return EXIT_FAILURE;
   }
 }
@@ -177,7 +179,7 @@ int check_url( char* url )
    //const char url[] = "127.0.0.1/greet.php?name=John";
         if( parser.parse(url) )
         {
-            std::cout << parser.scheme() << "://" << parser.hostname() << std::endl;
+            std::cout << "Successfully parsed url " << url << "\nparser.scheme() = " << parser.scheme() << "\nparser.hostname() = " << parser.hostname() << std::endl;
             return EXIT_SUCCESS;
         }
         else
@@ -207,16 +209,23 @@ int check_url( char* url )
       );
   }*/
 
+  /*https://en.cppreference.com/w/cpp/container/array/swap
+template<class Os, class V> Os& operator<<(Os& os, const V& v) {
+    os << "{";
+    for (auto i : v) os << ' ' << i;
+    return os << " } ";
+}*/
+
   void write_OK_to_client()
   {
     auto self = shared_from_this();
-    std::array<char,2> msg_OK = { 'J', 'D' }; // jest dobrze
+    //std::array<char,17> msg_OK = { 'H','T','T','P','/','1','.','1',' ','2','0','0',' ','O','K','\r','\n' }; 
      //  lub
-    // data_from_server_[0] = 'J'; 
-    // data_from_server_[1] = 'D';
+    // data_from_server_[0] = 'O'; 
+    // data_from_server_[1] = 'K';
     async_write(
         client_,
-        buffer(msg_OK, 2),
+        buffer(msg_OK_, 17),
         [self](std::error_code ec, std::size_t /*n*/)
         {
           if (!ec)
@@ -235,6 +244,7 @@ int check_url( char* url )
   tcp::socket server_;
   std::array<char, 1024> data_from_client_;
   std::array<char, 1024> data_from_server_;
+  std::array<char,17> msg_OK_ = { 'H','T','T','P','/','1','.','1',' ','2','0','0',' ','O','K','\r','\n' }; 
 
   const char* request_text_ = "GET /uri.cgi HTTP/1.1\r\n"
                         "User-Agent: Mozilla/5.0\r\n"
