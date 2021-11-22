0ff6161edf6ff8cc808d5ba3a6eeef5960c6f422
Pawel Lechowicz <plechowicz@endego.com>
2021-11-22 10:56:15 +0100

    new_client, async_server

--- src/server/new_server.cpp
+++ src/server/new_server.cpp
@@ -21,7 +21,7 @@ void send_(tcp::socket & socket, const string& message) {
 int main() {
       boost::asio::io_service io_service;
 //listen for new connection
-      tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 1234 ));
+      tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 5005 ));
 //socket creation 
       tcp::socket socket_(io_service);
 //waiting for connection
@@ -31,46 +31,7 @@ int main() {
       cout << message << endl;
 //write operation
       send_(socket_, "Hello From Server!");
-      cout << "Servent sent Hello message to Client!" << endl;
+      cout << "Server sent Hello message to Client!" << endl;
    return 0;
 }
 
-//////////////////client
-
-#include <iostream>
-#include <boost/asio.hpp>
-
-using namespace boost::asio;
-using ip::tcp;
-using std::string;
-using std::cout;
-using std::endl;
-
-int main() {
-     boost::asio::io_service io_service;
-//socket creation
-     tcp::socket socket(io_service);
-//connection
-     socket.connect( tcp::endpoint( boost::asio::ip::address::from_string("127.0.0.1"), 1234 ));
-// request/message from client
-     const string msg = "Hello from Client!\n";
-     boost::system::error_code error;
-     boost::asio::write( socket, boost::asio::buffer(msg), error );
-     if( !error ) {
-        cout << "Client sent hello message!" << endl;
-     }
-     else {
-        cout << "send failed: " << error.message() << endl;
-     }
- // getting response from server
-    boost::asio::streambuf receive_buffer;
-    boost::asio::read(socket, receive_buffer, boost::asio::transfer_all(), error);
-    if( error && error != boost::asio::error::eof ) {
-        cout << "receive failed: " << error.message() << endl;
-    }
-    else {
-        const char* data = boost::asio::buffer_cast<const char*>(receive_buffer.data());
-        cout << data << endl;
-    }
-    return 0;
-}
