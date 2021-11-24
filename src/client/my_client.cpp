94d56e6d13423ee8fd78645314ed2061f3d8b5cb
Paweł Iwaneczko <p.iwaneczko@kontakt.io>
2021-11-24 15:59:03 +0100

    Changes after review

--- src/client/my_client.cpp
+++ src/client/my_client.cpp
@@ -1,154 +1,159 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <boost/asio.hpp>
+#include <asio.hpp>
 
 #include <stdio.h>
 #include <ansi_colors.h>
 
-using boost::asio::ip::tcp;
+using asio::ip::tcp;
 
-enum { max_length = 1024 };
+enum
+{
+    max_length = 1024
+};
 
 class client
 {
 private:
-    // boost::asio::streambuf request;
+    // asio::streambuf request;
     // std::ostream request_stream;
     std::string server_;
     short port_;
     char request_[max_length];
     int request_length_;
-  
-public:
 
-client( std::string server, short port ):
-        server_(server), 
-        port_(port)
-{
-    boost::asio::io_service io_service;
+public:
+    client(std::string server, short port) : server_(server),
+                                             port_(port)
+    {
+        asio::io_service io_service;
 
         //socket creation
-    tcp::socket socket(io_service);
+        tcp::socket socket(io_service);
         //connection
-    socket.connect( tcp::endpoint( boost::asio::ip::address::from_string(server), port ));
-      
-    do_menu(); // wyświetl menu, przygotuj żądanie i ustaw request_length_
+        socket.connect(tcp::endpoint(asio::ip::address::from_string(server), port));
 
-    while (request_length_ > 0)  // wybrano z menu coś poza "Koniec"
-    {
-        if ( !socket.is_open() )
+        do_menu(); // wyświetl menu, przygotuj żądanie i ustaw request_length_
+
+        while (request_length_ > 0) // wybrano z menu coś poza "Koniec"
         {
-            // zamiast socket.open, bo nie znam parametrów
-            socket.connect( tcp::endpoint( boost::asio::ip::address::from_string(server), port ));
-        }
-        
-        //std::string request_str=request_;
-        boost::system::error_code error;
+            if (!socket.is_open())
+            {
+                // zamiast socket.open, bo nie znam parametrów
+                socket.connect(tcp::endpoint(asio::ip::address::from_string(server), port));
+            }
+
+            //std::string request_str=request_;
+            asio::error_code error;
             // Send request from client
-        boost::asio::write(socket, boost::asio::buffer(request_, (size_t)request_length_), error);
-        if( !error ) 
-        {
-            std::cout << BOLDBLUE << "\nClient sent a request:\n" << request_<< RESET << std::endl;
-        }
-        else 
-        {
-            std::cerr << RED << "\nClient sending request failed: " << error.message() << RESET << std::endl;
-        }
+            asio::write(socket, asio::buffer(request_, (size_t)request_length_), error);
+            if (!error)
+            {
+                std::cout << BOLDBLUE << "\nClient sent a request:\n"
+                          << request_ << RESET << std::endl;
+            }
+            else
+            {
+                std::cerr << RED << "\nClient sending request failed: " << error.message() << RESET << std::endl;
+            }
 
             // get response from server
-        boost::asio::streambuf response_buffer;
-        boost::asio::read(socket, response_buffer, boost::asio::transfer_all(), error);
-        if( error && error != boost::asio::error::eof ) 
-        {
-            std::cerr << RED << "Response from server failed: " << error.message() << RESET << std::endl;
+            asio::streambuf response_buffer;
+            asio::read(socket, response_buffer, asio::transfer_all(), error);
+            if (error && error != asio::error::eof)
+            {
+                std::cerr << RED << "Response from server failed: " << error.message() << RESET << std::endl;
+            }
+            else
+            {
+                const char *data = asio::buffer_cast<const char *>(response_buffer.data());
+                std::cout << BOLDGREEN << "Client got the following response:\n"
+                          << data << RESET << std::endl;
+            }
+
+            do_menu(); // wybierz coś z menu, przygotuj żądanie i ustaw request_length_
+            socket.close();
         }
-        else 
-        {
-            const char* data = boost::asio::buffer_cast<const char*>(response_buffer.data());
-            std::cout << BOLDGREEN << "Client got the following response:\n" << data << RESET << std::endl;
-        }
-
-        do_menu(); // wybierz coś z menu, przygotuj żądanie i ustaw request_length_
-        socket.close();
-    } 
-    std::cout << "End of client" << std::endl;
-} 
- 
-virtual ~client(){}
-
-// Wyświetla menu, pobiera odpowiedź, generuje żądanie,
-// i zwraca długość łancucha z żądaniem
-int do_menu()
-{
-    static int menu_cnt = 0;
-    int len, post_html_len, selection, user_temp, nr_temperatury;
-    char post_html_buf[500];
-    do
-    {
-        std::cout << "\n\n*=*=*=* " << ++menu_cnt << " *=*=*=*\n\n";
-        std::cout << " 1. Get temp 1\n";
-        std::cout << " 2. Get temp 2\n";
-        std::cout << " 3. Post temp 1\n";
-        std::cout << " 4. Post temp 2\n";
-        std::cout << " 5. Hello\n";
-        std::cout << " 6. Exit\n";
-        std::cout << "Wybierz: " << BOLDMAGENTA;
-        std::cin >> selection;
-        std::cout << RESET;
-
-        switch (selection)
-        {
-        case 1:
-        case 2:
-            len = sprintf( request_,         
-                "GET temp%d HTTP/1.1\r\n"
-                "Host: 127.0.0.1 \r\n"
-                "Accept: */*\r\n"
-                "Connection: close\r\n\r\n", selection );
-            break;
-        
-        case 3:
-        case 4:
-            nr_temperatury = selection - 2;
-            std::cout << "Podaj temperature " << nr_temperatury << ' ';
-            std::cin >> user_temp;
-          
-            post_html_len = sprintf( post_html_buf,         
-             "%d", user_temp );  
-          
-            len = sprintf( request_,  
-                "POST temp%d HTTP/1.1\r\n"
-                "Host: 127.0.0.1 \r\n"
-                "Accept: */*\r\n"
-                "Content-Type: application/x-www-form-urlencoded\r\n"
-                "Content-Length: %d\r\n"
-                "Connection: close\r\n\r\n" 
-                "%s", nr_temperatury, post_html_len, post_html_buf );         
-            break;
-
-        case 5:
-            len = sprintf( request_, "Hello\r\n" );
-            break;
-
-        case 6:
-            len = -1;
-            break;
-        
-        default:
-            len = 0;
-            std::cout << "\n Wybierz 1..6\n";
-            break;
-        }
-       
-    } while ( len == 0 );    // wymuś wybranie czegoś z menu poza default
-    request_length_ = std::max( len, 0 );
-    if( len != -1 )
-    {
-        std::cout << BLUE << "Client request_: " << request_  << RESET << std::endl;
+        std::cout << "End of client" << std::endl;
     }
-   return len;
-}
+
+    virtual ~client() {}
+
+    // Wyświetla menu, pobiera odpowiedź, generuje żądanie,
+    // i zwraca długość łancucha z żądaniem
+    int do_menu()
+    {
+        static int menu_cnt = 0;
+        int len, post_html_len, selection, user_temp, nr_temperatury;
+        char post_html_buf[500];
+        do
+        {
+            std::cout << "\n\n*=*=*=* " << ++menu_cnt << " *=*=*=*\n\n";
+            std::cout << " 1. Get temp 1\n";
+            std::cout << " 2. Get temp 2\n";
+            std::cout << " 3. Post temp 1\n";
+            std::cout << " 4. Post temp 2\n";
+            std::cout << " 5. Hello\n";
+            std::cout << " 6. Exit\n";
+            std::cout << "Wybierz: " << BOLDMAGENTA;
+            std::cin >> selection;
+            std::cout << RESET;
+
+            switch (selection)
+            {
+            case 1:
+            case 2:
+                len = sprintf(request_,
+                              "GET temp%d HTTP/1.1\r\n"
+                              "Host: 127.0.0.1 \r\n"
+                              "Accept: */*\r\n"
+                              "Connection: close\r\n\r\n",
+                              selection);
+                break;
+
+            case 3:
+            case 4:
+                nr_temperatury = selection - 2;
+                std::cout << "Podaj temperature " << nr_temperatury << ' ';
+                std::cin >> user_temp;
+
+                post_html_len = sprintf(post_html_buf,
+                                        "%d", user_temp);
+
+                len = sprintf(request_,
+                              "POST temp%d HTTP/1.1\r\n"
+                              "Host: 127.0.0.1 \r\n"
+                              "Accept: */*\r\n"
+                              "Content-Type: application/x-www-form-urlencoded\r\n"
+                              "Content-Length: %d\r\n"
+                              "Connection: close\r\n\r\n"
+                              "%s",
+                              nr_temperatury, post_html_len, post_html_buf);
+                break;
+
+            case 5:
+                len = sprintf(request_, "Hello\r\n");
+                break;
+
+            case 6:
+                len = -1;
+                break;
+
+            default:
+                len = 0;
+                std::cout << "\n Wybierz 1..6\n";
+                break;
+            }
+
+        } while (len == 0); // wymuś wybranie czegoś z menu poza default
+        request_length_ = std::max(len, 0);
+        if (len != -1)
+        {
+            std::cout << BLUE << "Client request_: " << request_ << RESET << std::endl;
+        }
+        return len;
+    }
 
 }; // class client
 
@@ -156,7 +161,7 @@ int main(void)
 {
     std::string server("127.0.0.1");
     short port = 5005;
-     
+
     client c(server, port);
     return 0;
 }
--- src/server/my_server.cpp
+++ src/server/my_server.cpp
@@ -1,217 +1,225 @@
 #include <cstdlib>
 #include <iostream>
-#include <boost/bind.hpp>
-#include <boost/asio.hpp>
+#include <asio.hpp>
 #include <ansi_colors.h>
 
 #include <src/httpparser/request.h>
 #include <src/httpparser/httprequestparser.h>
 
-using boost::asio::ip::tcp;
+using asio::ip::tcp;
 
 using httpparser::HttpRequestParser;
 using httpparser::Request;
 
-enum { max_length = 1024 };
+enum
+{
+    max_length = 1024
+};
 
 class server
 {
-  public:
-  server(short port):
- port_(port), ind1(0), ind2(0)
- {}
- virtual ~server(){}
+public:
+    server(short port) : port_(port), ind1(0), ind2(0)
+    {
+    }
+    virtual ~server() {}
 
-void run()
-{
- boost::asio::io_service io_service;
-      //listen for new connection
- tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), port_ ));
-      //socket creation 
- tcp::socket socket_(io_service);
+    void run()
+    {
+        asio::io_service io_service;
+        //listen for new connection
+        tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), port_));
+        //socket creation
+        tcp::socket socket_(io_service);
 
- std::string response_str;
- while (true)
- {
-          //waiting for connection
-      acceptor_.accept(socket_);
-    
-          //read request from client    
-      std::string request_str = read_(socket_);
-      std::cout << YELLOW << "Server got the following request:\n" << request_str << RESET << std::endl;
+        std::string response_str;
+        while (true)
+        {
+            //waiting for connection
+            acceptor_.accept(socket_);
 
-          // check request, prepare response
-      response_length_ = prepare_response( request_str );
-      response_str = response_;
+            //read request from client
+            std::string request_str = read_(socket_);
+            std::cout << YELLOW << "Server got the following request:\n"
+                      << request_str << RESET << std::endl;
 
-          //write response
-      send_(socket_, response_str);
-      std::cout << GREEN << "Server sent response:\n" << response_str << RESET << std::endl;
+            // check request, prepare response
+            response_length_ = prepare_response(request_str);
+            response_str = response_;
 
-      socket_.close();
- }
-}
+            //write response
+            send_(socket_, response_str);
+            std::cout << GREEN << "Server sent response:\n"
+                      << response_str << RESET << std::endl;
+
+            socket_.close();
+        }
+    }
 
 private:
-  //  char request_[max_length];
-  //  int request_length_;
+    //  char request_[max_length];
+    //  int request_length_;
     char response_[max_length];
     int response_length_;
     short port_;
-    
-  //tcp::acceptor acceptor_;
-  //tcp::socket socket_;
-  enum { temp_len = 5 };
+
+    //tcp::acceptor acceptor_;
+    //tcp::socket socket_;
+    enum
+    {
+        temp_len = 5
+    };
     // tablice temperatur
-  int temp1[temp_len] = { 2, 3, 5, 7, 11 };
-  int temp2[temp_len] = { 4, 6, 10, 14, 22 };
-  int ind1, ind2;
-  
-  int get_temp1()
-  {
-      int ret = temp1[ind1];
-      ind1 = (ind1 + 1) % temp_len;
-      return ret;
-  }
-  
-  int get_temp2()
-  {
-      int ret = temp2[ind2];
-      ind2 = (ind2 + 1) % temp_len;
-      return ret;
-  }
- 
- // set current element in temp1 
-  void set_temp1( int temp )
-  {
-      temp1[ind1] = temp;
-  }
-  
-  // set current element in temp2
-  void set_temp2( int temp )
-  {
-      temp2[ind2] = temp;
-  }
+    int temp1[temp_len] = {2, 3, 5, 7, 11};
+    int temp2[temp_len] = {4, 6, 10, 14, 22};
+    int ind1, ind2;
 
-//std::string STR_response;
-// Parsuje żądanie i umieszcza odpowiedź w response_. Zwraca długość odpowiedzi
-// 
-int prepare_response( std::string str_req ) //char * request_, int request_length_ )
-{
-  int nr, temp, len, html_len;
-  Request request;
-  HttpRequestParser parser;
-  bool OK = false;
-  bool Parsing_OK = false;
-  char response_html[500];
+    int get_temp1()
+    {
+        int ret = temp1[ind1];
+        ind1 = (ind1 + 1) % temp_len;
+        return ret;
+    }
 
-  auto request_ = str_req.data();
-  int request_length_ = str_req.length();
-  len = 0;
-  HttpRequestParser::ParseResult res = parser.parse(request, request_, request_ + request_length_);
-  if( res == HttpRequestParser::ParsingCompleted )
-  {
-      std::cout << "Request parsing completed " << request.inspect() << std::endl;
-      Parsing_OK = true;
-  }
-  else
-  {
-      std::cerr << RED << "Request parsing failed" << RESET << std::endl;
-      std::cerr << "request length = " << request_length_ << std::endl;
-  }
+    int get_temp2()
+    {
+        int ret = temp2[ind2];
+        ind2 = (ind2 + 1) % temp_len;
+        return ret;
+    }
 
-  if( Parsing_OK )
-  {
-  if( request.method == "GET" )
-  {
-         if ( request.uri == "temp1")
-         {
-             temp = get_temp1();
-             nr = 1;
-             OK = true;
-         } 
-         else if ( request.uri == "temp2" )
-         {
-             temp = get_temp2();
-             nr = 2;
-             OK = true;
-         } 
+    // set current element in temp1
+    void set_temp1(int temp)
+    {
+        temp1[ind1] = temp;
+    }
 
-         if( OK )
-         {
-          html_len = sprintf( response_html,         
-            "<html><body><h2>Temperatura %d = %d</h2></body></html>", 
-            nr, temp );
+    // set current element in temp2
+    void set_temp2(int temp)
+    {
+        temp2[ind2] = temp;
+    }
 
-           len = sprintf( response_,         
-            "HTTP/1.1 200 OK\r\n"
-            "Server: localhost\r\n"
-            "Content-Type: text/html\r\n"
-            "Content-Length: %d\r\n"
-            "Connection: close\r\n"
-            "\r\n%s",
-            html_len, response_html); 
-         }
-  }
-  else  if( request.method == "POST" )
-  {
-       if ( request.uri == "temp1" || request.uri == "temp2")
-       {
-            std::string temp_str(request.content.begin(), request.content.end());
-            temp = std::stoi(temp_str);
-            OK = true;
-            if ( request.uri == "temp1" )
+    //std::string STR_response;
+    // Parsuje żądanie i umieszcza odpowiedź w response_. Zwraca długość odpowiedzi
+    //
+    int prepare_response(std::string str_req) //char * request_, int request_length_ )
+    {
+        int nr, temp, len, html_len;
+        Request request;
+        HttpRequestParser parser;
+        bool OK = false;
+        bool Parsing_OK = false;
+        char response_html[500];
+
+        auto request_ = str_req.data();
+        int request_length_ = str_req.length();
+        len = 0;
+        HttpRequestParser::ParseResult res = parser.parse(request, request_, request_ + request_length_);
+        if (res == HttpRequestParser::ParsingCompleted)
+        {
+            std::cout << "Request parsing completed " << request.inspect() << std::endl;
+            Parsing_OK = true;
+        }
+        else
+        {
+            std::cerr << RED << "Request parsing failed" << RESET << std::endl;
+            std::cerr << "request length = " << request_length_ << std::endl;
+        }
+
+        if (Parsing_OK)
+        {
+            if (request.method == "GET")
             {
-                nr = 1;
-                set_temp1( temp );
+                if (request.uri == "temp1")
+                {
+                    temp = get_temp1();
+                    nr = 1;
+                    OK = true;
+                }
+                else if (request.uri == "temp2")
+                {
+                    temp = get_temp2();
+                    nr = 2;
+                    OK = true;
+                }
+
+                if (OK)
+                {
+                    html_len = sprintf(response_html,
+                                       "<html><body><h2>Temperatura %d = %d</h2></body></html>",
+                                       nr, temp);
+
+                    len = sprintf(response_,
+                                  "HTTP/1.1 200 OK\r\n"
+                                  "Server: localhost\r\n"
+                                  "Content-Type: text/html\r\n"
+                                  "Content-Length: %d\r\n"
+                                  "Connection: close\r\n"
+                                  "\r\n%s",
+                                  html_len, response_html);
+                }
             }
-            else
+            else if (request.method == "POST")
             {
-                nr = 2;
-                set_temp2( temp );
+                if (request.uri == "temp1" || request.uri == "temp2")
+                {
+                    std::string temp_str(request.content.begin(), request.content.end());
+                    temp = std::stoi(temp_str);
+                    OK = true;
+                    if (request.uri == "temp1")
+                    {
+                        nr = 1;
+                        set_temp1(temp);
+                    }
+                    else
+                    {
+                        nr = 2;
+                        set_temp2(temp);
+                    }
+
+                    html_len = sprintf(response_html,
+                                       "<html><body><h2>Ustawiono temperature %d na %d</h2></body></html>",
+                                       nr, temp);
+
+                    len = sprintf(response_,
+                                  "HTTP/1.1 200 OK\r\n"
+                                  "Server: localhost\r\n"
+                                  "Content-Type: text/html\r\n"
+                                  "Content-Length: %d\r\n"
+                                  "Connection: close\r\n"
+                                  "\r\n%s",
+                                  html_len, response_html);
+                }
             }
-        
-         html_len = sprintf( response_html,         
-            "<html><body><h2>Ustawiono temperature %d na %d</h2></body></html>", 
-            nr, temp );
+        }
+        if (!OK || !Parsing_OK)
+        {
+            len = sprintf(response_,
+                          "HTTP/1.1 400 Bad Request\r\n"
+                          "Server: localhost\r\n"
+                          "\r\n");
+        }
+        response_length_ = len;
+        //STR_response.append(response_);
+        return len;
+    } // prepare_response
 
-         len = sprintf( response_,         
-            "HTTP/1.1 200 OK\r\n"
-            "Server: localhost\r\n"
-            "Content-Type: text/html\r\n"
-            "Content-Length: %d\r\n"
-            "Connection: close\r\n"
-            "\r\n%s",
-            html_len, response_html); 
-       }
-  }
-  }
-  if( !OK || !Parsing_OK )
-  { 
-       len = sprintf( response_,         
-            "HTTP/1.1 400 Bad Request\r\n"
-            "Server: localhost\r\n"
-            "\r\n" );
+    std::string read_(tcp::socket &socket)
+    {
+        asio::streambuf buf;
+        asio::read_until(socket, buf, "\n");
+        std::string data = asio::buffer_cast<const char *>(buf.data());
+        return data;
+    }
 
-  }
-  response_length_ = len;
-  //STR_response.append(response_);
-  return len;
-} // prepare_response
+    void send_(tcp::socket &socket, const std::string &message)
+    {
+        const std::string msg = message + "\n";
+        asio::write(socket, asio::buffer(message));
+    }
 
-std::string read_(tcp::socket & socket) {
-       boost::asio::streambuf buf;
-       boost::asio::read_until( socket, buf, "\n" );
-       std::string data = boost::asio::buffer_cast<const char*>(buf.data());
-       return data;
-}
-
-void send_(tcp::socket & socket, const std::string& message) {
-       const std::string msg = message + "\n";
-       boost::asio::write( socket, boost::asio::buffer(message) );
-}
-
-};  // class server
+}; // class server
 
 int main()
 {
